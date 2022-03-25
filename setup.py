7c0b07f6f1f1b7498f76f5a62af2980c2f14135b
Licini <leochen1990@gmail.com>
2022-03-25 15:01:10 +0100

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_notebook",
-    version="0.2.1",
+    version="0.3.0",
     description="Display compas geometries in jupyter notebook.",
     long_description=long_description,
     long_description_content_type="text/markdown",
